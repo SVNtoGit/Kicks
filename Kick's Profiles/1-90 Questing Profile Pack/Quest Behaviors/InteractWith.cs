1647f94c1acc00f357bb305a24066f0434894f76
chinajade247@gmail.com <chinajade247@gmail.com@3e3cfd9d-047e-4510-b76e-1705b38c3900>
2013-04-20 10:20:09 +0000

    BUGFIX: InteractWith.cs * Repaired problem with new MinRange feature. From Tumbum's report: http://www.thebuddyforum.com/honorbuddy-forum/submitted-profiles/50549-kicks-mega-profile-packs-1-90-a-4.html#post1127123

--- Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/InteractWith.cs
+++ Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/InteractWith.cs
@@ -495,7 +495,7 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
 
             const double rangeEpsilon = 3.0;
             UsageCheck_SemanticCoherency(xElement,
-                ((RangeMax - RangeMin) < RangeMinMaxEpsilon),
+                Args.Keys.Contains("MinRange") && ((RangeMax - RangeMin) < RangeMinMaxEpsilon),
                 context => string.Format("Range({0}) must be at least {1} greater than MinRange({2}).",
                                         RangeMax, RangeMinMaxEpsilon, RangeMin)); 
         }
@@ -517,14 +517,14 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
         private int GossipPageIndex { get; set; }
         private HuntingGroundsType HuntingGrounds { get; set; }
         private WoWItem ItemToUse { get; set; }
-        private double RangeMinMaxEpsilon = 3.0;
+        private const double RangeMinMaxEpsilon = 3.0;
         private WoWObject SelectedInteractTarget { get; set; }
 
-        private WaitTimer _waitTimerAfterInteracting = new WaitTimer(TimeSpan.Zero);
+        private readonly WaitTimer _waitTimerAfterInteracting = new WaitTimer(TimeSpan.Zero);
 
         // DON'T EDIT THESE--they are auto-populated by Subversion
-        public override string SubversionId { get { return ("$Id: InteractWith.cs 456 2013-04-20 09:32:04Z chinajade $"); } }
-        public override string SubversionRevision { get { return ("$Revision: 456 $"); } }
+        public override string SubversionId { get { return ("$Id: InteractWith.cs 457 2013-04-20 10:19:03Z chinajade $"); } }
+        public override string SubversionRevision { get { return ("$Revision: 457 $"); } }
         #endregion
 
 
