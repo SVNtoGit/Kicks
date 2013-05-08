37076e4da82482ec645386e21e4b88ab7e08ab02
chinajade247@gmail.com <chinajade247@gmail.com@3e3cfd9d-047e-4510-b76e-1705b38c3900>
2013-05-08 14:27:58 +0000

    FIXUP FOR .557 REDROP BORKS * New comment 'place holder' files that overwrite quest behaviors in the new .557 distribution. This prevents symbol conflicts

--- trunk/Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/Development/Extensions_WoWObject.cs
+++ trunk/Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/Development/Extensions_WoWObject.cs
@@ -1,5 +1,5 @@
-﻿// The extensions contained in this file have been distributed to the QuestBehaviorCore/Extensions/
-// subdirectory to make them more managable.
+﻿// The previous contents of this file have been distributed to the QuestBehaviorCore
+// subdirectory tree to make them more managable.
 //
 // This comment file has been left in place to prevent 'symbol conflicts' for users that do
 // 'upgrade installs' of Honorbuddy.  The current Honorbuddy at the time of this writing is .557.
