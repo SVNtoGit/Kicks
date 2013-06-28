1a5d715f454d672823cc62dcc5e22534bb028804
chinajade247@gmail.com <chinajade247@gmail.com@3e3cfd9d-047e-4510-b76e-1705b38c3900>
2013-06-28 19:33:40 +0000

    UPDATE: InteractWith.cs * Removed support for ItemId as legacy name for InteractByUsingItemId.

--- Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/InteractWith.cs
+++ Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/InteractWith.cs
@@ -427,9 +427,7 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
                     ?? false;
                 InteractByQuestFrameAction = GetAttributeAsNullable<QuestFrameDisposition>("InteractByQuestFrameDisposition", false, null, null)
                     ?? QuestFrameDisposition.TerminateProfile;
-                InteractByUsingItemId = GetAttributeAsNullable<int>("InteractByUsingItemId", false, ConstrainAs.ItemId, null)
-                    ?? GetAttributeAsNullable<int>("ItemId", false, ConstrainAs.ItemId, null) /*Legacy name--don't use */
-                    ?? 0;
+                InteractByUsingItemId = GetAttributeAsNullable<int>("InteractByUsingItemId", false, ConstrainAs.ItemId, null) ?? 0;
 
                 // Tunables...
                 BuyItemCount = GetAttributeAsNullable<int>("BuyItemCount", false, ConstrainAs.CollectionCount, null) ?? 1;
@@ -578,8 +576,8 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
         private WaitTimer _timerToReachDestination = null;
 
         // DON'T EDIT THESE--they are auto-populated by Subversion
-        public override string SubversionId { get { return ("$Id: InteractWith.cs 575 2013-06-28 19:23:23Z chinajade $"); } }
-        public override string SubversionRevision { get { return ("$Revision: 575 $"); } }
+        public override string SubversionId { get { return ("$Id: InteractWith.cs 576 2013-06-28 19:33:06Z chinajade $"); } }
+        public override string SubversionRevision { get { return ("$Revision: 576 $"); } }
         #endregion
 
 
