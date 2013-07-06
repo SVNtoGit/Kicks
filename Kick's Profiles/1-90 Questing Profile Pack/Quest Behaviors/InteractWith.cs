bf19be5ea74c1c4e1f01a2a42765082157497985
chinajade247@gmail.com <chinajade247@gmail.com@3e3cfd9d-047e-4510-b76e-1705b38c3900>
2013-07-06 02:40:25 +0000

    UPDATE: InteractWith.cs * Corrected presence of MobState="BelowHp" being considered a 'semantic error' (which halted the profile).  It is now considered a 'deprecated' attribute (which just emits a warning).

--- Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/InteractWith.cs
+++ Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/InteractWith.cs
@@ -359,7 +359,6 @@ using Styx;
 using Styx.Common.Helpers;
 using Styx.CommonBot;
 using Styx.CommonBot.Frames;
-using Styx.CommonBot.POI;
 using Styx.CommonBot.Profiles;
 using Styx.Helpers;
 using Styx.Pathing;
@@ -531,6 +530,13 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
                 "ObjectType",
                 context => "The ObjectType attribute is no longer used by InteractWith."
                             + "  You may safely remove it from the profile call to the InteractWith behavior.");
+
+            UsageCheck_DeprecatedAttribute(xElement,
+                MobState == MobStateType.BelowHp,
+                "MobState=\"BelowHp\"",
+                context => "Please remove the 'MobState=\"BelowHp\"' attribute."
+                            + "  The \"BelowHp\" value is no longer used, and has been deprecated."
+                            + "  The \"MobHpPercentLeft\" attribute alone is sufficient to capture intent.");
         }
 
 
@@ -549,12 +555,6 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
                 MobIdIncludesSelf && !((InteractByCastingSpellId > 0) || (InteractByUsingItemId > 0)),
                 context => "When \"MobIdIncludesSelf\" is specified, one of the following attributes must also be specified:"
                             + "InteractByCastingSpellId, InteractByUsingItemId");
-
-            UsageCheck_SemanticCoherency(xElement,
-                MobState == MobStateType.BelowHp,
-                context => "Please remove the 'MobState=\"BelowHp\"' attribute."
-                            + "  The \"BelowHp\" value is no longer used, and has been deprecated."
-                            + "  The \"MobHpPercentLeft\" attribute alone is sufficient to capture intent.");
         }
         #endregion
 
@@ -582,8 +582,8 @@ namespace Honorbuddy.Quest_Behaviors.InteractWith
         private WaitTimer _timerToReachDestination = null;
 
         // DON'T EDIT THESE--they are auto-populated by Subversion
-        public override string SubversionId { get { return ("$Id: InteractWith.cs 587 2013-07-03 09:15:58Z chinajade $"); } }
-        public override string SubversionRevision { get { return ("$Revision: 587 $"); } }
+        public override string SubversionId { get { return ("$Id: InteractWith.cs 589 2013-07-06 02:39:23Z chinajade $"); } }
+        public override string SubversionRevision { get { return ("$Revision: 589 $"); } }
         #endregion
 
 
