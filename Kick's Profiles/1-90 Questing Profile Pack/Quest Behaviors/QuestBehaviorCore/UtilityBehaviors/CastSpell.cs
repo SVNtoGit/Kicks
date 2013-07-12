09655f93a494c0bebbc147b2d40642742551ab9b
chinajade247@gmail.com <chinajade247@gmail.com@3e3cfd9d-047e-4510-b76e-1705b38c3900>
2013-07-12 18:32:07 +0000

    UPDATES: Repaired some issues with CombatUseItemOnV2 target filters.

--- Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/QuestBehaviorCore/UtilityBehaviors/CastSpell.cs
+++ Kick's Profiles/1-90 Questing Profile Pack/Quest Behaviors/QuestBehaviorCore/UtilityBehaviors/CastSpell.cs
@@ -11,7 +11,6 @@
 #region Usings
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 using CommonBehaviors.Actions;
 using Styx;
@@ -28,37 +27,32 @@ namespace Honorbuddy.QuestBehaviorCore
 {
     public partial class UtilityBehaviorSeq
     {
-        public class UseItem : Sequence
+        /// <summary>
+        /// <para>Uses item defined by WOWITEMDELEGATE on target defined by SELECTEDTARGETDELEGATE.</para>
+        /// <para>Notes:<list type="bullet">
+        /// <item><description><para>* It is up to the caller to assure that all preconditions have been met for
+        /// using the item (i.e., the target is in range, the item is off cooldown, etc).</para></description></item>
+        /// <item><description><para> * If item use was successful, BT is provided with RunStatus.Success;
+        /// otherwise, RunStatus.Failure is returned (e.g., item is not ready for use,
+        /// item use was interrupted by combat, etc).</para></description></item>
+        /// <item><description><para>* It is up to the caller to blacklist the target, or select a new target
+        /// after successful item use.</para></description></item>
+        /// </list></para>
+        /// </summary>
+        /// <param name="selectedTargetDelegate"> may NOT be null.  The target provided by the delegate should be viable.</param>
+        /// <param name="wowItemDelegate"> may NOT be null.  The item provided by the delegate should be viable, and ready for use.</param>
+        /// <returns></returns>
+        public class CastSpell : Sequence
         {
-            /// <summary>
-            /// <para>Uses item defined by WOWITEMDELEGATE on target defined by SELECTEDTARGETDELEGATE.</para>
-            /// <para>Notes:<list type="bullet">
-            /// <item><description><para>* It is up to the caller to assure that all preconditions have been met for
-            /// using the item (i.e., the target is in range, the item is off cooldown, etc).</para></description></item>
-            /// <item><description><para> * If item use was successful, BT is provided with RunStatus.Success;
-            /// otherwise, RunStatus.Failure is returned (e.g., item is not ready for use,
-            /// item use was interrupted by combat, etc).</para></description></item>
-            /// <item><description><para>* It is up to the caller to blacklist the target, or select a new target
-            /// after successful item use.</para></description></item>
-            /// </list></para>
-            /// </summary>
-            /// <param name="selectedTargetDelegate">may NOT be null.  The target provided by the delegate should be viable.</param>
-            /// <param name="wowItemIdDelegate">may NOT be null.  The item provided by the delegate should be viable, and ready for use.</param>
-            /// <param name="actionOnMissingItemDelegate"></param>
-            /// <param name="actionOnSuccessfulItemUseDelegate"></param>
-            /// <returns></returns>
-            public UseItem(ProvideIntDelegate wowItemIdDelegate,
-                            ProvideWoWObjectDelegate selectedTargetDelegate,
-                            Action<object> actionOnMissingItemDelegate,
-                            Action<object, WoWObject> actionOnSuccessfulItemUseDelegate = null)
+            public CastSpell(ProvideIntDelegate spellIdDelegate,
+                             ProvideWoWObjectDelegate selectedTargetDelegate,
+                             Action<object, WoWObject> actionOnSuccessfulSpellCastDelegate = null)
             {
+                Contract.Requires(spellIdDelegate != null, context => "spellDelegate != null");
                 Contract.Requires(selectedTargetDelegate != null, context => "selectedTargetDelegate != null");
-                Contract.Requires(wowItemIdDelegate != null, context => "wowItemDelegate != null");
-                Contract.Requires(actionOnMissingItemDelegate != null, context => "actionOnMissingItemDelegate != null");
 
-                ActionOnMissingItemDelegate = actionOnMissingItemDelegate;
-                ActionOnSuccessfulItemUseDelegate = actionOnSuccessfulItemUseDelegate ?? ((context, wowObject) => { /*NoOp*/ });
-                WowItemIdDelegate = wowItemIdDelegate;
+                ActionOnSuccessfulSpellCastDelegate = actionOnSuccessfulSpellCastDelegate ?? ((context, wowObject) => { /*NoOp*/ });
+                SpellIdDelegate = spellIdDelegate;
                 SelectedTargetDelegate = selectedTargetDelegate;
 
                 Children = CreateChildren();
@@ -66,14 +60,13 @@ namespace Honorbuddy.QuestBehaviorCore
 
 
             // BT contruction-time properties...
-            private Action<object> ActionOnMissingItemDelegate { get; set; }
-            private Action<object, WoWObject> ActionOnSuccessfulItemUseDelegate { get; set; }
-            private ProvideIntDelegate WowItemIdDelegate { get; set; }
+            private Action<object, WoWObject> ActionOnSuccessfulSpellCastDelegate { get; set; }
+            private ProvideIntDelegate SpellIdDelegate { get; set; }
             private ProvideWoWObjectDelegate SelectedTargetDelegate { get; set; }
 
             // BT visit-time properties...
-            private WoWItem CachedItemToUse { get; set; }
-            private WoWObject CachedTarget { get; set; }
+            private WoWSpell CachedSpell { get; set; }
+            private WoWUnit CachedTarget { get; set; }
             private bool IsInterrupted { get; set; }
 
 
@@ -81,36 +74,49 @@ namespace Honorbuddy.QuestBehaviorCore
             {
                 return new List<Composite>()
                 {
+                    // TODO: Break this up...
+                    // TODO: Install 'unknown spell' failure hook...
+
                     // Cache & qualify...
                     new Action(context =>
                     {
-                        CachedTarget = SelectedTargetDelegate(context);
-                        if (!Query.IsViable(CachedTarget))
+                        // If SelectedTargetDelegate is null, then its implicitly 'self', or a general effect...
+                        var selectedTarget = SelectedTargetDelegate(context) ?? Me;
+
+                        // Valid target?
+                        CachedTarget = selectedTarget as WoWUnit;
+                        if (selectedTarget != null)
                         {
-                            QBCLog.Warning("Target is not viable!");
+                            CachedTarget = selectedTarget as WoWUnit;
+                            if (CachedTarget == null)
+                            {
+                                QBCLog.Warning("Target {0} is not a WoWUnit--cannot cast spell on it", selectedTarget.SafeName());
+                                return RunStatus.Failure;
+                            }
+                        }
+
+                        // Spell known?
+                        var spellId = SpellIdDelegate(context);
+                        SpellFindResults findSpellResults;
+                        var isSpellFound = SpellManager.FindSpell(spellId, out findSpellResults);
+                        if (!isSpellFound)
+                        {
+                            QBCLog.Warning("Spell {0} is not known.", Utility.GetSpellNameFromId(spellId));
                             return RunStatus.Failure;
                         }
 
-                        if (!Query.IsViable(CachedItemToUse))
+                        // Spell not usable?
+                        CachedSpell = findSpellResults.Override ?? findSpellResults.Original;
+                        if (!SpellManager.CanCast(CachedSpell))
                         {
-                            var itemId = WowItemIdDelegate(context);
-                            CachedItemToUse = Me.CarriedItems.FirstOrDefault(i => (i.Entry == itemId));
-                            if (!Query.IsViable(CachedItemToUse))
-                                { QBCLog.Error("{0} is not in our bags.", Utility.GetItemNameFromId(itemId)); }
+                            QBCLog.Warning("{0} is not usable, yet.  (cooldown: {1})",
+                                CachedSpell.Name,
+                                Utility.PrettyTime(TimeSpan.FromSeconds((int)CachedSpell.CooldownTimeLeft.TotalSeconds)));
                             return RunStatus.Failure;
                         }
 
                         return RunStatus.Success;
                     }),
-
-                    // Wait for Item cooldown...
-                    new DecoratorContinue(context => !CachedItemToUse.Usable,
-                        new ActionFail(context =>
-                        {
-                            TreeRoot.StatusText = string.Format("{0} is not usable, yet. (cooldown remaining: {1})",
-                                CachedItemToUse.Name,
-                                Utility.PrettyTime(TimeSpan.FromSeconds((int)CachedItemToUse.CooldownTimeLeft.TotalSeconds)));
-                        })),
 
                     // Use the item...
                     new Action(context =>
@@ -122,22 +128,18 @@ namespace Honorbuddy.QuestBehaviorCore
                         InterruptDetection_Hook();
 
                         // Notify user of intent...
-                        var message = string.Format("Attempting use of '{0}' on '{1}'", CachedItemToUse.Name, CachedTarget.SafeName());
+                        var message = string.Format("Attempting cast of '{0}' on '{1}'", CachedSpell.Name, CachedTarget.SafeName());
 
-                        var selectedTargetAsWoWUnit = CachedTarget as WoWUnit;
-                        if (selectedTargetAsWoWUnit != null)
-                        {
-                            if (selectedTargetAsWoWUnit.IsDead)
-                                { message += " (dead)"; }
-                            else
-                                { message += string.Format(" (health: {0:F1})", selectedTargetAsWoWUnit.HealthPercent); }
-                        }
+                        message +=
+                            (CachedTarget.IsDead)
+                            ? " (dead)"
+                            : string.Format(" (health: {0:F1})", CachedTarget.HealthPercent);
 
                         QBCLog.DeveloperInfo(message);
 
                         // Do it...
                         IsInterrupted = false;
-                        CachedItemToUse.Use(CachedTarget.Guid);
+                        SpellManager.Cast(CachedSpell, CachedTarget);
                     }),
                     new WaitContinue(Delay.AfterItemUse, context => false, new ActionAlwaysSucceed()),
 
@@ -165,18 +167,19 @@ namespace Honorbuddy.QuestBehaviorCore
                     new Action(context => { InterruptDectection_Unhook(); }),
                     new DecoratorContinue(context => IsInterrupted,
                         new Sequence(
-                            new Action(context => { QBCLog.Warning("Use of {0} interrupted.", CachedItemToUse.Name); }),
+                            new Action(context => { QBCLog.Warning("Cast of {0} interrupted.", CachedSpell.Name); }),
                             // Give whatever issue encountered a chance to settle...
                             // NB: Wait, not WaitContinue--we want the Sequence to fail when delay completes.
                             new Wait(TimeSpan.FromMilliseconds(1500), context => false, new ActionAlwaysFail())
                         )),
                     new Action(context =>
                     {
-                        QBCLog.DeveloperInfo("Use of '{0}' on '{1}' succeeded.", CachedItemToUse.Name, CachedTarget.SafeName());
-                        ActionOnSuccessfulItemUseDelegate(context, CachedTarget);
+                        QBCLog.DeveloperInfo("Cast of '{0}' on '{1}' succeeded.", CachedSpell.Name, CachedTarget.SafeName());
+                        ActionOnSuccessfulSpellCastDelegate(context, CachedTarget);
                     })
                 };
             }
+
 
             private void HandleInterrupted(object sender, LuaEventArgs args)
             {
